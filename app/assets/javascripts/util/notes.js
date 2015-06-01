1316e0d726c0b57a073031d760de31663a4c69f7
Holyoke <kid.idioteque@gmail.com>
2015-06-01 10:16:59 -0700

    Work on note.js, add constructor and helpers

---
+++ app/assets/javascripts/util/notes.js
@@ -0,0 +1,36 @@
+function () {
+
+  var ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+  var createOscillator = function(freq){
+     var osc = ctx.createOscillator();
+     osc.type = "sine";
+     osc.frequency.value = freq;
+     osc.detune.value = 0;
+     osc.start(ctx.currentTime);
+
+     return osc;
+   };
+
+   var createGainNode = function(){
+     var gainNode = ctx.createGain();
+     gainNode.gain.value = 0;
+     gainNode.connect(ctx.destination);
+
+     return gainNode;
+   };
+
+  window.Note = function(freq){
+    this.freq = freq;
+
+    this.oscillatorNode = createOscillator(freq);
+    this.gainNode = gainNode();
+    this.oscillatorNode.connect(this.gainNode);
+  };
+
+  Note.prototype.start = function (){
+    
+  };
+
+
+}();
