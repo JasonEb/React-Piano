8825beeae33e547e755500a93829fdb545fcb5f9
Holyoke <kid.idioteque@gmail.com>
2015-06-02 20:26:09 -0700

    Revert back

--- app/assets/javascripts/components/janko.js.jsx
+++ app/assets/javascripts/components/janko.js.jsx
@@ -1,39 +1,36 @@
 var Janko = React.createClass({
   render: function() {
     return (
-      <div className="janko-keyboard">
+      <div className="keyboard">
 
-        <div className='black-keys'>
-          <span className='group-one'>
-            <Key noteName='C#4' qwertyKey='W'/>
+        <div className='row-one'>
+          <span className = 'row-keys'>
+            <Key noteName='C4' qwertyKey='A'/>
+            <Key noteName='D4' qwertyKey='S'/>
+            <Key noteName='E4' qwertyKey='D'/>
+            <Key noteName='F#4' qwertyKey='F'/>
+            <Key noteName='G#4' qwertyKey='G'/>
+            <Key noteName='A4' qwertyKey='H'/>
+            <Key noteName='B4' qwertyKey='J'/>
+            <Key noteName='C5' qwertyKey='K'/>
+            <Key noteName='D5' qwertyKey='L'/>
+            <Key noteName='E5' qwertyKey=';'/>
           </span>
-            <Key noteName='Eb4' qwertyKey='E'/>
-
-          <span className='group-two'>
-            <Key noteName='F#4' qwertyKey='T'/>
-            <Key noteName='G#4' qwertyKey='Y'/>
-            <Key noteName='Bb4' qwertyKey='U'/>
-          </span>
-
-          <span className='group-three'>
-            <Key noteName='C#5' qwertyKey='O'/>
-            <Key noteName='Eb5' qwertyKey='P'/>
-          </span>
-
-
         </div>
 
-        <div className="white-keys">
-          <Key noteName='C4' qwertyKey='A'/>
-          <Key noteName='D4' qwertyKey='S'/>
-          <Key noteName='E4' qwertyKey='D'/>
-          <Key noteName='F4' qwertyKey='F'/>
-          <Key noteName='G4' qwertyKey='G'/>
-          <Key noteName='A4' qwertyKey='H'/>
-          <Key noteName='B4' qwertyKey='J'/>
-          <Key noteName='C5' qwertyKey='K'/>
-          <Key noteName='D5' qwertyKey='L'/>
-          <Key noteName='E5' qwertyKey=';'/>
+        <div className='row-two'>
+          <span className = 'row-keys'>
+            <Key noteName='C4' qwertyKey='A'/>
+            <Key noteName='D4' qwertyKey='S'/>
+            <Key noteName='E4' qwertyKey='D'/>
+            <Key noteName='F4' qwertyKey='F'/>
+            <Key noteName='G4' qwertyKey='G'/>
+            <Key noteName='A4' qwertyKey='H'/>
+            <Key noteName='B4' qwertyKey='J'/>
+            <Key noteName='C5' qwertyKey='K'/>
+            <Key noteName='D5' qwertyKey='L'/>
+            <Key noteName='E5' qwertyKey=';'/>
+          </span>
         </div>
 
       </div>
