7f0585ec4248ee48eacd69eaf5a6cacf51b6017d
Holyoke <kid.idioteque@gmail.com>
2015-06-04 17:12:10 -0700

    Update production rb for assets deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  config.assets.compile = true
+  config.assets.digest = true
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
